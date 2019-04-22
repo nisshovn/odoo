611b2c5f5d4f8954cacacd0fea32a54be3dcd3b2
nevthanhps <thanhps@nissho-vn.com>
2019-04-22 09:40:31 +0700

    [ADD] Add translation text

--- odoo/addons/soj_warnings/models/product_aging.py
+++ odoo/addons/soj_warnings/models/product_aging.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.fields import Date as fDate
 from dateutil import relativedelta
 
@@ -9,7 +9,7 @@ class ProductAging(models.Model):
     _inherit = 'stock.quant'
 
     # Product Age Category
-    product_age_cat = fields.Char(string='Age Category', default='0 - 3 months')
+    product_age_cat = fields.Char(string='Age Category', default=_('0 - 3 months'))
 
     # TODO: Create overdue date to store the date of expired => for product age warning popup
     # overdue_date = fields.Date(string='Overdue date', compute='_compute_overdue', store=True)
@@ -20,26 +20,26 @@ class ProductAging(models.Model):
         self.search([
             ('in_date', '>', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=3))),
         ]).write({
-            'product_age_cat': '0 - 3 months'
+            'product_age_cat': _('0 - 3 months')
         })
 
         self.search([
             ('in_date', '<=', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=3))),
             ('in_date', '>', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=6))),
         ]).write({
-            'product_age_cat': '3 - 6 months'
+            'product_age_cat': _('3 - 6 months')
         })
 
         self.search([
             ('in_date', '<=', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=6))),
             ('in_date', '>', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=12))),
         ]).write({
-            'product_age_cat': '6 - 12 months'
+            'product_age_cat': _('6 - 12 months')
         })
 
         self.search([
             ('in_date', '<=', fields.Date.to_string(fDate.today() - relativedelta.relativedelta(months=12))),
         ]).write({
-            'product_age_cat': 'Over 12 months'
+            'product_age_cat': _('Over 12 months')
         })
         return True
